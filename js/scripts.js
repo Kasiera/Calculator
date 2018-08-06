2b55161852583b2e0415f85954295ed216230b4d
Samuel Kasiera <kasieras@gmail.com>
2018-08-06 17:48:56 +0300

    Added calculation codes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,32 @@
-var add= function(number 1, number 2){
-return number1+number2;
+var add = function(number1, number2) {
+  return number1 + number2;
 };
-var number1=parseInt(prompt("Enter a number:"));
-var number2=parseInt("Enter another number:"));
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+alert(result);
+var subtract= function(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = subtract(number1, number2);
+alert(result);
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = multiply(number1, number2);
+alert(result);
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = divide(number1, number2);
+alert(result);
