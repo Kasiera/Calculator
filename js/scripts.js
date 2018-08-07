934aefdfd293ff4d31011ec6d0d711767590394f
Samuel Kasiera <kasieras@gmail.com>
2018-08-07 08:09:51 +0300

    copy pasted last var

--- js/scripts.js
+++ js/scripts.js
@@ -30,3 +30,11 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = divide(number1, number2);
 alert(result);
+
+
+
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = divide(number1, number2);
+alert(result);
